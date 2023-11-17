99a80d115bf061b2fd996732c8f11b5b363bbda6
NicolasSlenko <101363647+NicolasSlenko@users.noreply.github.com>
2023-11-16 23:00:32 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
-apt install python3.8-venv -y
-python3.8 -m venv venv
+sudo apt-get install python3-venv -y
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python app.py
